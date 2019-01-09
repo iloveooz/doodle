da32627f70add2e1be392fb175b59b19c2771825
iloveooz <iloveooz@yandex.ru>
2019-01-09 21:55:03 +0700

    added jumping and moving platforms

--- main.cxx
+++ main.cxx
@@ -25,11 +25,11 @@
 		// спрайты
 		sf::Sprite sBackground(t1), sPlat(t2), sPers(t3);
 		
-		// массив точек
+		// массив платформ
 		point plat[20];
 		
 		// заполняем массив
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 8; i++) {
 			plat[i].x = rand() % 400;
 			plat[i].y = rand() % 533;
 		}
@@ -38,7 +38,7 @@
 		int y = 100;
 		int h = 200;
 		
-		float dx = 0.0;
+		// float dx = 0.0;
 		float dy = 0.0;
 		
 		while (app.isOpen()) {
@@ -59,13 +59,23 @@
 			
 			if (y > 500) dy = -10;
 			
+			if (y < h)
+				for (int i = 0; i < 8; i++) {
+					y = h;
+					plat[i].y = plat[i].y - dy;
+					if (plat[i].y > 533) {
+						plat[i].y = 0; 
+						plat[i].x = rand() % 400;
+					}
+				}   
+			
 			// диагностические сообщения
 			std::cout << "x = " << x << ", y = " << y << ", dy = " << dy << std::endl;
 			
 			// чтобы дудл отскакивал от площадки
-			for (int i = 0; i < 10; i++) 
+			for (int i = 0; i < 8; i++) 
 				if ((x + 50 > plat[i].x) &&
-					(x + 20 < plat[i].x + 68) &&
+					(x + 30 < plat[i].x + 68) &&
 					(y + 70 > plat[i].y) &&
 					(y + 70 < plat[i].y + 14) &&
 					(dy > 0))
@@ -76,7 +86,7 @@
 			app.draw(sBackground);
 			app.draw(sPers);
 			
-			for (int i = 0; i < 10; i++) {
+			for (int i = 0; i < 8; i++) {
 				sPlat.setPosition(plat[i].x, plat[i].y);
 				app.draw(sPlat);
 			}
