8777d82779ccb6c46452ce08a280c953e8614dd3
iloveooz <iloveooz@yandex.ru>
2019-01-09 17:06:02 +0700

    added jumping, looks awesome

--- main.cxx
+++ main.cxx
@@ -1,6 +1,7 @@
 
 	#include <SFML/Graphics.hpp>
 	#include <time.h>
+	#include <iostream>
 	
 	struct point {
 		int x;
@@ -27,10 +28,18 @@
 		// массив точек
 		point plat[20];
 		
+		// заполняем массив
 		for (int i = 0; i < 10; i++) {
 			plat[i].x = rand() % 400;
 			plat[i].y = rand() % 533;
 		}
+		
+		int x = 100;
+		int y = 100;
+		int h = 200;
+		
+		float dx = 0.0;
+		float dy = 0.0;
 		
 		while (app.isOpen()) {
 			sf::Event e;
@@ -39,6 +48,30 @@
 				if (e.type == sf::Event::Closed)
 					app.close();
 			}
+			
+			// управление
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) x += 3;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) x -= 3;
+			
+			// падение (интересно реализовано)
+			dy += 0.2; 
+			y += dy;
+			
+			if (y > 500) dy = -10;
+			
+			// диагностические сообщения
+			std::cout << "x = " << x << ", y = " << y << ", dy = " << dy << std::endl;
+			
+			// чтобы дудл отскакивал от площадки
+			for (int i = 0; i < 10; i++) 
+				if ((x + 50 > plat[i].x) &&
+					(x + 20 < plat[i].x + 68) &&
+					(y + 70 > plat[i].y) &&
+					(y + 70 < plat[i].y + 14) &&
+					(dy > 0))
+					dy = -10;
+			
+			sPers.setPosition(x, y);
 			
 			app.draw(sBackground);
 			app.draw(sPers);
